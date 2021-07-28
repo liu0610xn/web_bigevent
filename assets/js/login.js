dde80e4fb1623836de0863fef70becf8a555ea7b
刘逍男 <8342701+lxn0610@user.noreply.gitee.com>
2021-07-28 19:33:13 +0800

    完成了登录和注册功能开发

---
+++ assets/js/login.js
@@ -0,0 +1,58 @@
+$(function() {
+    $("#link_reg").on("click", function() {
+        $(".login-box").hide();
+        $(".reg-box").show();
+    })
+    $("#link_login").on("click", function() {
+            $(".login-box").show();
+            $(".reg-box").hide();
+        })
+        //从layui中获取form对象
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+            pwd: [
+                /^[\S]{6,12}$/,
+                '密码必须6到12位，且不能出现空格'
+            ],
+            repwd: function(value, item) {
+                var pwd = $(".reg-box [name=password]").val()
+                if (pwd !== value) {
+                    return '两次密码不一致'
+                }
+            }
+        })
+        //监听注册表单的提交事件
+    $("#form_reg").on("submit", function(e) {
+            e.preventDefault();
+            var data = {
+                username: $("#form_reg [name=username]").val(),
+                password: $("#form_reg [name=password]").val()
+            }
+            $.post("/api/reguser", data, function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg('注册成功，请登录');
+                $("#link_login").click();
+            })
+        })
+        // 监听表单登录的事件
+    $("#form_login").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            url: "/api/login",
+            data: "data",
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg("登录失败")
+                }
+                layer.msg("登录失败")
+                localStorage.setItem("token", res.token)
+                location.href = "/index.html"
+            }
+        });
+    })
+})
